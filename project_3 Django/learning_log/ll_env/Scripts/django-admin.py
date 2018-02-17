6bf0150d5517841dbcf825938923eeb3efd6ebd5
Your Name <you@example.com>
2018-02-17 22:52:01 +0800

    project_3 django

---
+++ project_3 Django/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a1718\desktop\python-crash-course\project_3 django\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project_3 Django/usage.md
@@ -0,0 +1,10 @@
+### 需要的包（ll_env/已安装）
+
+> pip install django
+> pip install django-bootstrap3
+
+### 运行
+
+> project_3 Django\learning_log>ll_env\Scripts\activate
+> project_3 Django\learning_log>manage.py runserver
+
